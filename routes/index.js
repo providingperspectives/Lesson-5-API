6547af6622ede1eedb7c5bf8859d6560b75358fc
isiakpona-chuks <providingperspectivesent@gmail.com>
2023-01-30 18:06:44 +0100

    api-fixed

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('user', (req, res) => {
+    res.status(200).json ((Response ={
+        reversed:req.query.name.split((''), reverse().join(''))
+    }))
+})
+
+
 router.use('/', require('./swagger'));
 
 
